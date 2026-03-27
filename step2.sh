d0e771844be27759451b26728a9991eae548f8bc
mskmei <3242904363@qq.com>
2026-03-28 01:56:24 +0900

    result

--- step2.sh
+++ step2.sh
@@ -1,7 +1,7 @@
 python IEMOCAP/inference_ejsl_frame.py \
   --frame_root /raid_zoe/home/lr/wangyi/sign/eJSL_dial/frame \
   --txt_root /raid_elmo/home/lr/wangyi/PTR/STUDIES-Japanese/Short_dialogue \
-  --save_model_root ./IEMOCAP/save_model \
+  --save_model_root /raid_zoe/home/lr/maokeyu/sign/open/IEMOCAP/save_model \
   --batch_size 4 \
   --num_workers 4 \
   --save_dir ./IEMOCAP/outputs_ejsl \
--- step5.sh
+++ step5.sh
@@ -3,6 +3,6 @@ python analysis/build_case_analysis.py \
   --txt_root /raid_elmo/home/lr/wangyi/PTR/STUDIES-Japanese/Short_dialogue \
   --telme_pred_csv ./IEMOCAP/outputs_ejsl/telme_ejsl_test_predictions.csv \
   --emotrans_pred_csv ./emotrans/saved/meld_dial_metrics/test_epoch001_predictions.csv \
-  --eanwh_pred_csv ./outputs_eanwh/eanwh_ejsl_test_predictions.csv \
+  --eanwh_pred_csv ./eanwh_res.csv \
   --out_dir ./analysis/outputs_cases \
   --expected_samples 1920
