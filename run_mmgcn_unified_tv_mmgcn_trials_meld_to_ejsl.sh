c07dd7d0ab26b088155714cbf04165a904086652
MAO KEYU <mao_keyu@outlook.com>
2026-08-31 03:05:42 +0900

    6

--- run_mmgcn_unified_tv_mmgcn_trials_meld_to_ejsl.sh
+++ run_mmgcn_unified_tv_mmgcn_trials_meld_to_ejsl.sh
@@ -14,7 +14,7 @@ BATCH_SIZE=${BATCH_SIZE:-8}
 EPOCHS=${EPOCHS:-10}
 LONGER_EPOCHS=${LONGER_EPOCHS:-$EPOCHS}
 MAX_GRAD_NORM=${MAX_GRAD_NORM:-5.0}
-TRIALS=${TRIALS:-"stable weighted_nll"}
+TRIALS=${TRIALS:-"stable_seed41 stable_seed42 stable_seed47 stable_lr15 stable_dropout25 stable_gamma10"}
 
 mkdir -p "$OUT_ROOT"
 
@@ -38,10 +38,12 @@ run_trial() {
   local dropout="$5"
   local loss="$6"
   local focal_gamma="$7"
+  local l2="$8"
+  shift 8
   local trial_out="$OUT_ROOT/$name"
 
   mkdir -p "$trial_out"
-  echo "[MMGCN-TV-TRIALS][$name] seed=$seed epochs=$epochs lr=$lr dropout=$dropout loss=$loss focal_gamma=$focal_gamma"
+  echo "[MMGCN-TV-TRIALS][$name] seed=$seed epochs=$epochs lr=$lr l2=$l2 dropout=$dropout loss=$loss focal_gamma=$focal_gamma extra=$*"
 
   CUDA_VISIBLE_DEVICES="$GPU" python MMGCN/train_eval_mmgcn_unified.py \
     --train_pkl "$MELD_UNIFIED_PKL" \
@@ -52,30 +54,68 @@ run_trial() {
     --epochs "$epochs" \
     --batch_size "$BATCH_SIZE" \
     --lr "$lr" \
+    --l2 "$l2" \
     --dropout "$dropout" \
     --loss "$loss" \
     --focal_gamma "$focal_gamma" \
     --max_grad_norm "$MAX_GRAD_NORM" \
-    --seed "$seed" | tee "$trial_out/train.log"
+    --seed "$seed" \
+    "$@" | tee "$trial_out/train.log"
 }
 
 for trial in $TRIALS; do
   case "$trial" in
     stable)
-      run_trial stable 43 "$EPOCHS" 0.0002 0.30 focal 1.5
+      run_trial stable 43 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00003
       ;;
     weighted_nll)
-      run_trial weighted_nll 44 "$EPOCHS" 0.0002 0.30 nll 2.0
+      run_trial weighted_nll 44 "$EPOCHS" 0.0002 0.30 nll 2.0 0.00003
       ;;
     seed_only)
-      run_trial seed_only 45 "$EPOCHS" 0.0003 0.40 focal 2.0
+      run_trial seed_only 45 "$EPOCHS" 0.0003 0.40 focal 2.0 0.00003
       ;;
     longer)
-      run_trial longer 46 "$LONGER_EPOCHS" 0.00015 0.35 focal 1.5
+      run_trial longer 46 "$LONGER_EPOCHS" 0.00015 0.35 focal 1.5 0.00003
+      ;;
+    stable_seed41)
+      run_trial stable_seed41 41 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00003
+      ;;
+    stable_seed42)
+      run_trial stable_seed42 42 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00003
+      ;;
+    stable_seed47)
+      run_trial stable_seed47 47 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00003
+      ;;
+    stable_lr15)
+      run_trial stable_lr15 43 "$EPOCHS" 0.00015 0.30 focal 1.5 0.00003
+      ;;
+    stable_lr25)
+      run_trial stable_lr25 43 "$EPOCHS" 0.00025 0.30 focal 1.5 0.00003
+      ;;
+    stable_dropout25)
+      run_trial stable_dropout25 43 "$EPOCHS" 0.0002 0.25 focal 1.5 0.00003
+      ;;
+    stable_dropout35)
+      run_trial stable_dropout35 43 "$EPOCHS" 0.0002 0.35 focal 1.5 0.00003
+      ;;
+    stable_gamma10)
+      run_trial stable_gamma10 43 "$EPOCHS" 0.0002 0.30 focal 1.0 0.00003
+      ;;
+    stable_gamma20)
+      run_trial stable_gamma20 43 "$EPOCHS" 0.0002 0.30 focal 2.0 0.00003
+      ;;
+    stable_l2low)
+      run_trial stable_l2low 43 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00001
+      ;;
+    stable_l2high)
+      run_trial stable_l2high 43 "$EPOCHS" 0.0002 0.30 focal 1.5 0.0001
+      ;;
+    stable_nocw)
+      run_trial stable_nocw 43 "$EPOCHS" 0.0002 0.30 focal 1.5 0.00003 --no_class_weight
       ;;
     *)
       echo "[MMGCN-TV-TRIALS] unknown trial: $trial" >&2
-      echo "[MMGCN-TV-TRIALS] valid: stable weighted_nll seed_only longer" >&2
+      echo "[MMGCN-TV-TRIALS] valid: stable weighted_nll seed_only longer stable_seed41 stable_seed42 stable_seed47 stable_lr15 stable_lr25 stable_dropout25 stable_dropout35 stable_gamma10 stable_gamma20 stable_l2low stable_l2high stable_nocw" >&2
       exit 1
       ;;
   esac
